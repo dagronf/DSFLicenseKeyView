05ee6816333d9b3bc0536e0ed47a25115cf267c3
Darren Ford <dford_au-reg@yahoo.com>
2018-12-17 08:20:42 +1100

    Initial Commit

---
+++ DSFLIcenceField/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  DSFLIcenceField
+//
+//  Created by Darren Ford on 17/12/18.
+//  Copyright © 2018 Darren Ford. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface AppDelegate : NSObject <NSApplicationDelegate>
+
+
+@end
+
---
+++ DSFLIcenceField/main.m
@@ -0,0 +1,13 @@
+//
+//  main.m
+//  DSFLIcenceField
+//
+//  Created by Darren Ford on 17/12/18.
+//  Copyright © 2018 Darren Ford. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, const char * argv[]) {
+	return NSApplicationMain(argc, argv);
+}
